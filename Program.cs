e634eb59efed83452051eca3e859dae71196cf20
Liam Bolan <AQ231688@aquinas.ac.uk>
2023-09-27 10:21:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+             
+            DrawRectangle((ConsoleColor)c+5, 51, 13, 25, 11);
+            
+            DrawRectangle((ConsoleColor)c+1, 35, 15, 16, 3);
+           
+            DrawRectangle((ConsoleColor)c+2, 76, 15, 16, 3);
+            
+            DrawRectangle((ConsoleColor)c+3, 58, 8, 11, 5);
+
+            DrawRectangle((ConsoleColor)c+8, 54, 24, 5, 8);
+
+            DrawRectangle((ConsoleColor)c+6, 68, 24, 5, 8);
+
+            Console.ReadLine();
+        }
+
+        
+        static void DrawRectangle(ConsoleColor C, int x, int y, int w, int h)
+        {
+
+            int xpos = x;
+            int ypos = y;
+            Console.BackgroundColor = C;
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    Console.SetCursorPosition(xpos + i, ypos + j);
+                    Console.Write(" ");
+
+                }
+              
+            }
+        }
+
+        
+    }
+}
+
+
